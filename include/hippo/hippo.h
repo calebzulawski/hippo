c06bf67f19225acb93ca0b07dae56188f1a28982
Caleb Zulawski <caleb.zulawski@gmail.com>
2019-01-27 01:22:21 +0000

    Add printer prefixes and suffixes, and one-liner stringifiers

--- include/hippo/hippo.h
+++ include/hippo/hippo.h
@@ -8,14 +8,32 @@
 
 namespace hippo {
 
-template <typename T> struct printer {};
+template <typename T, typename U = T> struct printer;
+template <typename T, typename U = T> struct stringifier;
+
+template <typename T>
+struct stringifier<
+    T, typename std::enable_if<std::is_arithmetic<T>::value, T>::type> {
+  static std::string stringify(const T &t) { return std::to_string(t); }
+};
 
 namespace detail {
+
+template <typename, typename = std::void_t<>>
+struct has_stringifier : std::false_type {};
+
 template <typename T>
-std::vector<std::string> deduce_print(const T &t, std::uint64_t width,
-                                      std::uint64_t indent,
+struct has_stringifier<T,
+                       std::void_t<decltype(::hippo::stringifier<T>::stringify(
+                           std::declval<T>()))>> : std::true_type {};
+
+template <typename T>
+inline constexpr bool has_stringifier_v = has_stringifier<T>::value;
+
+template <typename T>
+std::vector<std::string> deduce_print(const T &t, std::uint64_t indent,
                                       std::uint64_t current_indent) {
-  return ::hippo::printer<T>(t, width, indent, current_indent);
+  return ::hippo::printer<T>::print(t, indent, current_indent);
 }
 
 std::vector<std::string> split_and_indent(const std::string &s,
@@ -29,51 +47,65 @@ std::vector<std::string> split_and_indent(const std::string &s,
   return lines;
 }
 
+template <typename T>
+void print_member(const char *name, const T &value,
+                  std::vector<std::string> &lines, std::uint64_t indent,
+                  std::uint64_t current_indent) {
+  std::string header(current_indent, ' ');
+  header.append(name);
+  if constexpr (::hippo::detail::has_stringifier<T>::value) {
+    header.push_back(' ');
+    header.append(::hippo::stringifier<T>::stringify(value));
+    lines.emplace_back(std::move(header));
+  } else {
+    auto prefix = ::hippo::printer<T>::prefix();
+    if (prefix.size() > 0)
+      header.append(prefix);
+    lines.emplace_back(std::move(header));
+    auto sublines =
+        ::hippo::detail::deduce_print(value, indent, current_indent + indent);
+    lines.insert(lines.end(), std::begin(sublines), std::end(sublines));
+    auto suffix = ::hippo::printer<T>::suffix();
+    if (suffix.size() > 0) {
+      std::string footer(current_indent, ' ');
+      footer.append(suffix);
+      lines.emplace_back(std::move(footer));
+    }
+  }
+}
+
 } // namespace detail
 
 template <typename T>
-std::vector<std::string> print(const T &t, std::uint64_t width,
-                               std::uint64_t indent) {
-  return detail::deduce_print(t, width, indent, 0);
+std::vector<std::string> print(const T &t, std::uint64_t indent) {
+  return detail::deduce_print(t, indent, 0);
 }
 
 } // namespace hippo
 
-#define DETAIL_HIPPO_LEADER(Name)                                              \
-  std::string leader(' ', current_indent);                                     \
-  leader.append(#Name ":");                                                    \
-  lines.emplace_back(leader);
-
 #define HIPPO_BEGIN(Type)                                                      \
   namespace hippo {                                                            \
   template <> struct printer<Type> {                                           \
-    std::vector<std::string> print(const Type &t, std::uint64_t width,         \
-                                   std::uint64_t indent,                       \
-                                   std::uint64_t current_indent) {             \
+    static std::string prefix() { return " {"; }                               \
+    static std::string suffix() { return "}"; }                                \
+    static std::vector<std::string> print(const Type &t, std::uint64_t indent, \
+                                          std::uint64_t current_indent) {      \
       std::vector<std::string> lines;
 
 #define HIPPO_END()                                                            \
+  return lines;                                                                \
   }                                                                            \
   }                                                                            \
   ;                                                                            \
   }
 
-#define HIPPO_MEMBER(Name, Expression)                                         \
+#define HIPPO_MEMBER_EXPR(Name, Expression)                                    \
   {                                                                            \
-    DETAIL_HIPPO_LEADER(Name);                                                 \
-    auto sublines = ::hippo::detail::deduce_print((Expression), width, indent, \
-                                                  current_indent + indent);    \
-    lines.insert(lines.end(), std::begin(sublines), std::end(sublines));       \
+    using Type = decltype(Expression);                                         \
+    ::hippo::detail::print_member(#Name ":", (Expression), lines, indent,      \
+                                  current_indent);                             \
   }
 
-#define HIPPO_MEMBER_STREAM(Name, Expression)                                  \
-  {                                                                            \
-    DETAIL_HIPPO_LEADER(Name);                                                 \
-    std::ostringstream ss;                                                     \
-    ss << std::string(' ', current_indent) << (Expression);                    \
-    auto sublines =                                                            \
-        ::hippo::detail::split_and_indent(ss.str(), current_indent);           \
-    lines.insert(lines.end(), std::begin(sublines), std::end(sublines));       \
-  }
+#define HIPPO_MEMBER(Name) HIPPO_MEMBER_EXPR(Name, t.Name)
 
 #endif // HIPPO_HIPPO_H_
--- test/test.cpp
+++ test/test.cpp
@@ -1,3 +1,56 @@
 #include "hippo/hippo.h"
+#include <iostream>
 
-int main() { return 0; };
+struct Bottom1 {
+  int a = 1;
+};
+
+HIPPO_BEGIN(Bottom1)
+HIPPO_MEMBER(a)
+HIPPO_END()
+
+struct Bottom2 {
+  int a = 1;
+  int b = 2;
+};
+
+HIPPO_BEGIN(Bottom2)
+HIPPO_MEMBER(a)
+HIPPO_MEMBER(b)
+HIPPO_END()
+
+struct Middle1 {
+  Bottom2 a;
+};
+
+HIPPO_BEGIN(Middle1)
+HIPPO_MEMBER(a)
+HIPPO_END()
+
+struct Middle2 {
+  Bottom1 a;
+  Bottom2 b;
+};
+
+HIPPO_BEGIN(Middle2)
+HIPPO_MEMBER(a)
+HIPPO_MEMBER(b)
+HIPPO_END()
+
+struct Top {
+  Middle1 a;
+  Middle2 b;
+};
+
+HIPPO_BEGIN(Top)
+HIPPO_MEMBER(a)
+HIPPO_MEMBER(b)
+HIPPO_END()
+
+int main() {
+  Top t;
+  auto lines = hippo::print(t, 2);
+  for (const auto &line : lines)
+    std::cout << line << std::endl;
+  return 0;
+};
